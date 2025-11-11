2ae6051f97eb32dcc9e01208eb654e2ccfdb4d6f
joaquinS25 <mitroncito25@hotmail.com>
2025-11-11 16:51:00 -0500

    CERRAR CAJA FUNCIONANDO CORRECTAMENTE

--- vista/v_venta_listar.php
+++ vista/v_venta_listar.php
@@ -10,42 +10,44 @@
     <div class="card mb-4">
         <div class="card-header"><i class="fas fa-table me-1"></i> Ventas</div>
         <div class="card-body">
-            <table id="datatablesSimple">
-                <thead>
-                    <tr>
-                        <th>#</th>
-                        <th>ID</th>
-                        <th>Fecha Venta</th>
-                        <th>Cantidad</th>
-                        <th>Producto</th>
-                        <th>Precio Total</th>
-                        <th>Efectivo</th>
-                        <th>Yape</th>
-                        <th>Medio de Pago</th>
-                        <th>Usuario</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <?php 
-                    $n=0;
-                    foreach ($ventas as $value) {
-                        $n++;
-                    ?>
-                    <tr>
-                        <td><?= $n ?></td>
-                        <td><?= $value['id_venta'] ?></td>
-                        <td><?= $value['fecha_venta'] ?></td>
-                        <td><?= $value['cantidad'] ?></td>
-                        <td><?= $value['nom_producto'] ?></td>
-                        <td><?= $value['precio_venta'] ?></td>
-                        <td><?= $value['precio_efectivo'] ?></td>
-                        <td><?= $value['precio_yape'] ?></td>
-                        <td><?= $value['nom_medio_pago'] ?></td>
-                        <td><?= $value['nom_usuario']." ".$value['ape_usuario'] ?></td>
-                    </tr>
-                    <?php } ?>
-                </tbody>
-            </table>
+            <div  class="">
+                <table id="datatablesSimple">
+                    <thead>
+                        <tr>
+                            <th>#</th>
+                            <th>ID</th>
+                            <th>Fecha Venta</th>
+                            <th>Cantidad</th>
+                            <th>Producto</th>
+                            <th>Precio Total</th>
+                            <th>Efectivo</th>
+                            <th>Yape</th>
+                            <th>Medio de Pago</th>
+                            <th>Usuario</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php 
+                        $n=0;
+                        foreach ($ventas as $value) {
+                            $n++;
+                        ?>
+                        <tr>
+                            <td><?= $n ?></td>
+                            <td><?= $value['id_venta'] ?></td>
+                            <td><?= $value['fecha_venta'] ?></td>
+                            <td><?= $value['cantidad'] ?></td>
+                            <td><?= $value['nom_producto'] ?></td>
+                            <td><?= $value['precio_venta'] ?></td>
+                            <td><?= $value['precio_efectivo'] ?></td>
+                            <td><?= $value['precio_yape'] ?></td>
+                            <td><?= $value['nom_medio_pago'] ?></td>
+                            <td><?= $value['nom_usuario']." ".$value['ape_usuario'] ?></td>
+                        </tr>
+                        <?php } ?>
+                    </tbody>
+                </table>
+            </div>
         </div>
     </div>
 </div>
